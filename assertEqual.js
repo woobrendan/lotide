04ab54efafbd100cb5c1805036b36268828c712c
Brendan Woo <woo.brendan@gmail.com>
2022-01-10 20:42:39 +0000

    add assertEqual doc, compares expected and actual input

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const exEmoji = String.fromCodePoint(0x274C);
+const checkEmoji = String.fromCodePoint(0x2705);
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log(checkEmoji + checkEmoji + " Assertaion Passes: " + actual + " === " + expected);
+  } else {
+    console.log(exEmoji + exEmoji + " Assertaion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("Hello", "hello");
+assertEqual(1, 5);
