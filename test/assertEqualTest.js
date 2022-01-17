84ce04e0f6b6c5cd9581e82afe239d11f783be99
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 18:14:31 +0000

    created and added test for head and assertEqual function. both importing the required functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("Hello", "hello");
+assertEqual(1, 5);
+assertEqual(55, 55);
