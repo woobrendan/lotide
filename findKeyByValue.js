1a111db8534fe81d16a38e33eabd224fd06ed8df
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 18:15:59 +0000

    added import of assertEqual function and removed the function declaration of same function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,4 @@
-const exEmoji = String.fromCodePoint(0x274C);
-const checkEmoji = String.fromCodePoint(0x2705);
-
-const assertEqual = function(actual, expected) {
-  if (expected === actual) {
-    console.log(`${checkEmoji}${checkEmoji} Assertaion Passes: ${actual} === ${expected}`);
-  } else {
-    console.log(`${exEmoji}${exEmoji} Assertaion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const findKeyByValue = function(someObject, someValue) {
@@ -30,4 +21,3 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
