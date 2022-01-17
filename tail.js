bf9d8010c1467476cab331ede28195dee0a4cdc6
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 18:23:10 +0000

    refactored, removed assertEqual, changed to arrow function from function declaration. created export of tail function

--- tail.js
+++ tail.js
@@ -1,19 +1,7 @@
-const exEmoji = String.fromCodePoint(0x274C);
-const checkEmoji = String.fromCodePoint(0x2705);
-const assertEqual = function(actual, expected) {
-  if (expected === actual) {
-    console.log(`${checkEmoji}${checkEmoji} Assertaion Passes: ${actual} === ${expected}`);
-  } else {
-    console.log(`${exEmoji}${exEmoji} Assertaion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual');
 
-const tail = function(Array) {
-  return Array.slice(1);
-};
+const tail = (Array) => Array.slice(1);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-let tailArray = tail(words);
-console.log(tailArray);
-assertEqual(words.length, 3);
+
+module.exports = tail;
+
