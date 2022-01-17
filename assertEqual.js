87d8892bf4aa1c910dbcf053604c114cc9e66ed1
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 18:12:13 +0000

    refactored, removed variable for emoji and hardcoded that, exported function for re-usability

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,9 @@
-const exEmoji = String.fromCodePoint(0x274C);
-const checkEmoji = String.fromCodePoint(0x2705);
 const assertEqual = function(actual, expected) {
   if (expected === actual) {
-    console.log(checkEmoji + checkEmoji + " Assertaion Passes: " + actual + " === " + expected);
+    console.log(`✅✅ Assertaion Passes: ${actual} === ${expected}`);
   } else {
-    console.log(exEmoji + exEmoji + " Assertaion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌ Assertaion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual("Hello", "hello");
-assertEqual(1, 5);
-assertEqual(55, 55);
+module.exports = assertEqual;
