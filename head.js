20d9c14d5e14ef6be5150b820fcbbf651e1589ae
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 18:13:20 +0000

    refactored, removing hard coding of assertEqual function, and now import that function for usage, and export head function

--- head.js
+++ head.js
@@ -1,17 +1,9 @@
-const exEmoji = String.fromCodePoint(0x274C);
-const checkEmoji = String.fromCodePoint(0x2705);
 
-const assertEqual = function(actual, expected) {
-  if (expected === actual) {
-    console.log(`${checkEmoji}${checkEmoji} Assertaion Passes: ${actual} === ${expected}`);
-  } else {
-    console.log(`${exEmoji}${exEmoji} Assertaion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function(Array) {
   return Array[0];
 };
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
