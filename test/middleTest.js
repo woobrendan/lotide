9d5bb18b746b19c5cca40a14b5a0acdf8c9b093a
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 20:34:11 +0000

    refactored to use chai instead of manual tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 const middle = require('../middle');
 
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4]);
-assertArraysEqual(middle([]), []);
+describe("#middle", () => {
+  it("returns [5] for [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5])
+  });
+  it("returns [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+  it("returns empty array when given empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
