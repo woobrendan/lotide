c36fb234b4c5cf54938767bd68336cf2fd5179d6
Brendan Woo <woo.brendan@gmail.com>
2022-01-17 19:26:18 +0000

    refactored, imported eqArray check, exported the assertArraysEqual func. changed emojis from code to img/str.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,42 +1,11 @@
-const exEmoji = String.fromCodePoint(0x274C);
-const checkEmoji = String.fromCodePoint(0x2705);
-const assertEqual = function(actual, expected) {
-  if (expected === actual) {
-    console.log(`${checkEmoji}${checkEmoji} Assertaion Passes: ${actual} === ${expected}`);
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅ These two arrays, ${arrayOne} and ${arrayTwo} are the same.`);
   } else {
-    console.log(`${exEmoji}${exEmoji} Assertaion Failed: ${actual} !== ${expected}`);
+    console.log(`❌❌ This first array ${arrayOne} is not the same as the second array ${arrayTwo}.`);
   }
 };
 
-const eqArrays = function(firstArray, secondArray) {
-  let checker;
-  let masterArray = [firstArray, secondArray]
-  let longestArrayLength = firstArray.length;
-  for (let array of masterArray) {
-    if (array.length > longestArrayLength) {
-      longestArrayLength = array.length;
-    } 
-  }
-  for (let i = 0; i < longestArrayLength; i++) {
-    if (firstArray[i] === secondArray[i]) {
-      checker = true;
-    } else return false;
-  }
-  return checker;
-};
-
-
-const assertArrayEqual = function(arrayOne, arrayTwo) {
-  
-  let same = eqArrays(arrayOne, arrayTwo);
-  if (same) {
-    console.log(`${checkEmoji}${checkEmoji} These two arrays, ${arrayOne} and ${arrayTwo} are the same.`);
-  } else {
-    console.log(`${exEmoji}${exEmoji} This first array ${arrayOne} is not the same as the second array ${arrayTwo}.`);
-  }
-};
-
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-
-
+module.exports = assertArraysEqual;
